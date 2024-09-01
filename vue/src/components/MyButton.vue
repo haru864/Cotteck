84c75937c895daeeaf45e14f57669424af9bb5d4
haru <kawachigenji95@gmail.com>
2024-09-01 19:57:57 +0900

    Run Vue.js on Docker

---
+++ vue/src/components/MyButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <button>{{ buttonText }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    setup() {
+        const buttonText = ref('MyButton');
+
+        return {
+            buttonText,
+        };
+    },
+});
+</script>
